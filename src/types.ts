6cd734a23d01620473ab08aa0661eb0951ab5549
zFlxw <58326890+zFlxw@users.noreply.github.com>
2022-07-04 17:26:47 +0200

    feat: Foundation (#3)
    * refactor: Types
    
    * feat: RGB to hex
    
    * feat: Testing with GitHub Actions workflow (#1)
    
    * feat: Tests for hex to rgb and rgb to hex
    
    * feat: Test workflow for GitHub actions
    
    * refactor: Color class and types (#2)

--- src/types.ts
+++ src/types.ts
@@ -1,92 +1,258 @@
 export type Colors =
-    | 'hex'
-    | 'rgb'
-    | 'rgba'
-    | 'hsl'
-    | 'hsla'
-    | 'hsv'
-    | 'hsva'
-    | 'hwb'
-    | 'hwba'
-    | 'cymk';
+    | HexColor
+    | RGBColor
+    | HSLColor
+    | HSBColor
+    | HWBColor
+    | CYMKColor;
 
-/** A color representation in hexadecimal format. */
-export interface HexColor {
-    /** The color in hexadecimal format. */
-    hex: string;
+/** A color in hex format. */
+export class HexColor {
+    /** The hex string */
+    private hex: string;
+
+    /** Creates a new hex color class. */
+    constructor(hex: string) {
+        this.hex = hex.replace('0x', '#');
+    }
+
+    /** Converts a hex color to a RGB color. */
+    public toRGB(): RGBColor {
+        const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(
+            this.hex,
+        );
+        if (result) {
+            const r = parseInt(result[1], 16);
+            const g = parseInt(result[2], 16);
+            const b = parseInt(result[3], 16);
+
+            return new RGBColor(r, g, b);
+        }
+
+        throw new Error('Invalid hex string found.');
+    }
+
+    /** Gets the hex value as string. */
+    public toString(): string {
+        return this.hex;
+    }
+
+    /** Gets the red value of color. */
+    public get red(): number {
+        return this.toRGB().red;
+    }
+
+    /** Gets the green value of color. */
+    public get green(): number {
+        return this.toRGB().green;
+    }
+
+    /** Gets the blue value of color. */
+    public get blue(): number {
+        return this.toRGB().blue;
+    }
 }
 
-/** A color representation in RGB format. */
-export interface RGBColor {
-    /** The red value of the color. */
-    r: number;
+/** A color in RGB format. */
+export class RGBColor {
+    private r: number;
+    private g: number;
+    private b: number;
+    private a?: number | undefined;
 
-    /** The green value of the color. */
-    g: number;
+    /**
+     * Creates a new RGB color.
+     * @param r The red value.
+     * @param g The green value.
+     * @param b The blue value.
+     * @param a The alpha value.
+     */
+    constructor(r: number, g: number, b: number, a?: number) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = a;
+    }
 
-    /** The blue value of the color. */
-    b: number;
+    /** Converts the RGB Color to a hex color. */
+    public toHex(): HexColor {
+        return new HexColor(
+            `#${((1 << 24) + (this.red << 16) + (this.green << 8) + this.blue)
+                .toString(16)
+                .slice(1)}`,
+        );
+    }
 
-    /** The alpha value of the color. */
-    a?: number;
+    /** Gets the red value. */
+    public get red(): number {
+        return this.r;
+    }
+
+    /** Gets the green value. */
+    public get green(): number {
+        return this.g;
+    }
+
+    /** Gets the blue value. */
+    public get blue(): number {
+        return this.b;
+    }
+
+    /** Gets the alpha value if set. */
+    public get alpha(): number | undefined {
+        return this.a;
+    }
+
+    /** Gets the RGBColor object as string. */
+    public toString(): string {
+        if (this.alpha) {
+            return `rgba(${this.red}, ${this.green}, ${this.blue}, ${this.alpha})`;
+        } else {
+            return `rgb(${this.red}, ${this.green}, ${this.blue})`;
+        }
+    }
 }
 
-/** A color representation in HSL format. */
-export interface HSLColor {
-    /** The hue value of the color in degrees. */
-    h: number;
+/** A color in HSL format. */
+export class HSLColor {
+    private h: number;
+    private s: number;
+    private l: number;
+    private a?: number;
 
-    /** The saturation value of the color in percent. */
-    s: number;
+    /** Creates a new HSL color. */
+    constructor(h: number, s: number, l: number, a?: number) {
+        this.h = h;
+        this.s = s;
+        this.l = l;
+        this.a = a;
+    }
 
-    /** The lightness value of the color in percent. */
-    l: number;
+    /** Gets the hue value of the color in degrees. */
+    public get hue(): number {
+        return this.h;
+    }
 
-    /** The alpha value of the color. */
-    a?: number;
+    /** Gets the saturation value of the color in percent. */
+    public get saturation(): number {
+        return this.s;
+    }
+
+    /** Gets the lightness value of the color in percent. */
+    public get lightness(): number {
+        return this.l;
+    }
+
+    /** Gets the alpha value of the color if set. */
+    public get alpha(): number | undefined {
+        return this.a;
+    }
 }
 
-/** A color representation in HSV format. */
-export interface HSVColor {
-    /** The hue value of the color in degrees. */
-    h: number;
+/** A color in HSB/HSV format. */
+export class HSBColor {
+    private h: number;
+    private s: number;
+    private b: number;
+    private a?: number;
 
-    /** The saturation value of the color in percent. */
-    s: number;
+    /** Creates a new HSB color. */
+    constructor(h: number, s: number, b: number, a?: number) {
+        this.h = h;
+        this.s = s;
+        this.b = b;
+        this.a = a;
+    }
 
-    /** The "value" (brightness) of the color in percent. */
-    v: number;
+    /** Gets the hue value of the color in degrees. */
+    public get hue(): number {
+        return this.h;
+    }
 
-    /** The alpha value of the color. */
-    a?: number;
+    /** Gets the saturation value of the color in percent. */
+    public get saturation(): number {
+        return this.s;
+    }
+
+    /** Gets the brightness (value) value of the color in percent. */
+    public get brightness(): number {
+        return this.b;
+    }
+
+    /** Gets the alpha value of the color if set. */
+    public get alpha(): number | undefined {
+        return this.a;
+    }
 }
 
-/** A color representation in HWB format. */
-export interface HWBColor {
-    /** The hue value of the color in degrees. */
-    h: number;
+/** A color in HWB format. */
+export class HWBColor {
+    private h: number;
+    private w: number;
+    private b: number;
+    private a?: number;
 
-    /** The whiteness value of the color in percent. */
-    w: number;
+    /** Creates a new HWB color. */
+    constructor(h: number, w: number, b: number, a?: number) {
+        this.h = h;
+        this.w = w;
+        this.b = b;
+        this.a = a;
+    }
 
-    /** The blackness value of the color in percent. */
-    b: number;
+    /** Gets the hue value of the color in degrees. */
+    public get hue(): number {
+        return this.h;
+    }
 
-    /** The alpha value of the color in percent. */
-    a?: number;
+    /** Gets the whiteness value of the color in percent. */
+    public get whiteness(): number {
+        return this.w;
+    }
+
+    /** Gets the blackness value of the color in percent. */
+    public get blackness(): number {
+        return this.b;
+    }
+
+    /** Gets the alpha value of the color if set. */
+    public get alpha(): number | undefined {
+        return this.a;
+    }
 }
 
-/** A color representation in CYMK format. */
-export interface CYMKColor {
-    /** The cyan value of the color. */
-    c: number;
+/** A color in CYMK format. */
+export class CYMKColor {
+    private c: number;
+    private y: number;
+    private m: number;
+    private k: number;
 
-    /** The yellow value of the color. */
-    y: number;
+    /** Creates a new CYMK color. */
+    constructor(c: number, y: number, m: number, k: number) {
+        this.c = c;
+        this.y = y;
+        this.m = m;
+        this.k = k;
+    }
 
-    /** The magenta value of the color. */
-    m: number;
+    /** Gets the cyan value of the color. */
+    public get cyan(): number {
+        return this.c;
+    }
 
-    /** The key (black) value of the color. */
-    k: number;
+    /** Gets the yellow value of the color. */
+    public get yellow(): number {
+        return this.y;
+    }
+
+    /** Gets the magenta value of the color. */
+    public get magenta(): number {
+        return this.m;
+    }
+
+    /** Gets the key (black) value of the color. */
+    public get key(): number {
+        return this.k;
+    }
 }
